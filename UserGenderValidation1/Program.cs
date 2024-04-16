0c91b29261acf9a0003c861ba3b09161f9fc36b0
Mihkel Pinsel. <mihkel.pinsel@tktk.ee>
2024-04-16 15:49:45 +0300

    add week2 code

---
+++ UserGenderValidation1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome To Instagram.");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to usse Instagram.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not rad your age.");
+}
---
+++ Week2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//uued andmetüübid
+Console.WriteLine("Hello, World!");
